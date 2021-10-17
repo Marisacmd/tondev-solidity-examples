108a688af74cfe1222c8b165261d0b89b9a81f67
Tamara Kobyakova <kobyakovatamarah@gmail.com>
2021-10-17 13:54:37 +0500

    small formatting changes

--- Multiplication_example.sol
+++ Multiplication_example.sol
@@ -1,28 +1,28 @@
-pragma ton-solidity >= 0.35.0;
+pragma ton - solidity >= 0.35 .0;
 pragma AbiHeader expire;
 
 contract Multiplication {
 
-	uint storedData=1;
+    uint storedData = 1;
 
-	constructor() public {
-		require(tvm.pubkey() != 0, 101);
-		require(msg.pubkey() == tvm.pubkey(), 102);
-		tvm.accept();
-	}
-
-	modifier checkOwnerAndAccept {
-		require(msg.pubkey() == tvm.pubkey(), 102);
-		tvm.accept();
-		_;
-	}
-
-    function multiplyStoredNumberByMyNumber(uint x) public checkOwnerAndAccept{
-	require(1 <= x && x <= 10);
-        storedData = storedData*x;
+    constructor() public {
+        require(tvm.pubkey() != 0, 101);
+        require(msg.pubkey() == tvm.pubkey(), 102);
+        tvm.accept();
     }
 
-    function getStoredNumber() public checkOwnerAndAccept view returns (uint) {
+    modifier checkOwnerAndAccept {
+        require(msg.pubkey() == tvm.pubkey(), 102);
+        tvm.accept();
+        _;
+    }
+
+    function multiplyStoredNumberByMyNumber(uint x) public checkOwnerAndAccept {
+        require(1 <= x && x <= 10);
+        storedData = storedData * x;
+    }
+
+    function getStoredNumber() public checkOwnerAndAccept view returns(uint) {
         return storedData;
     }
 
--- Queue_example.sol
+++ Queue_example.sol
@@ -1,33 +1,33 @@
-pragma ton-solidity >= 0.35.0;
+pragma ton - solidity >= 0.35 .0;
 pragma AbiHeader expire;
 
 contract Queue {
 
-        string[] storedData = [];
+    string[] storedData = [""];
 
-	constructor() public {
-		require(tvm.pubkey() != 0, 101);
-		require(msg.pubkey() == tvm.pubkey(), 102);
-		tvm.accept();
-	}
+    constructor() public {
+        require(tvm.pubkey() != 0, 101);
+        require(msg.pubkey() == tvm.pubkey(), 102);
+        tvm.accept();
+    }
 
-	modifier checkOwnerAndAccept {
-		require(msg.pubkey() == tvm.pubkey(), 102);
-		tvm.accept();
-		_;
-	}
+    modifier checkOwnerAndAccept {
+        require(msg.pubkey() == tvm.pubkey(), 102);
+        tvm.accept();
+        _;
+    }
 
-    function putHimInQueue(string x) public checkOwnerAndAccept{
+    function putHimInQueue(string x) public checkOwnerAndAccept {
         storedData.push(x);
     }
 
-    function callNext() public checkOwnerAndAccept{
+    function callNext() public checkOwnerAndAccept {
         storedData[0] = storedData[storedData.length - 1];
         storedData.pop();
-    
+
     }
 
-    function viewQueue() public checkOwnerAndAccept view returns (string[]){
+    function viewQueue() public checkOwnerAndAccept view returns(string[]) {
         return storedData;
     }
 
